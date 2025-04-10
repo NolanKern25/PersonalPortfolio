ac70a0e649c6e60d124843caa4a2536f3ad25335
Nolan Kern <nolankern24@gmail.com>
2025-04-10 14:33:43 -0400

    file creating

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-
-import './App.css'
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Nolan/Nolan";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className={styles.App}>
+      <Navbar />
+      <Nolan />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
