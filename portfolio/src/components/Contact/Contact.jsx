add850b7cb23f5951dd39d7bd14c47e6742c62c2
Nolan Kern <nolankern24@gmail.com>
2025-04-15 14:01:11 -0400

    contact finished

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
+import email from '../../assets/contact/emailIcon.png';
+import linkedin from '../../assets/contact/linkedinIcon.png';
+import github from '../../assets/contact/githubIcon.png';
 
 export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <p>Reach out at any time!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <img src={email} alt="" />
+          <a href="mailto:nkern@ashland.edu">Email</a>
         </li>
         <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <img src={linkedin} alt=""/>
+            <a href="https://www.linkedin.com/in/nolan-kern25/">LinkedIn</a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <img src={github} alt="" />
+          <a href="https://www.github.com/NolanKern25">Github</a>
         </li>
       </ul>
     </footer>
